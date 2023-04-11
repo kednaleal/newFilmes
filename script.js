297a1441bb2b032e7da46e77630db68fb31fe63e
kednaleal <capic.2021218minf0175@aluno.ifpi.edu.br>
2023-04-11 12:59:29 -0300

    o mais recente

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-function getNewFilmes(titulo){
-    const txt =  document.getElementById('pesquisa')
-    const buttao = document.getElementById('procuraB')
-    const capa = document.getElementById('capa')
-    const titulo = document.getElementById('titulo')
-    const descricao = document.getElementById('descricao')
-
-
-
+function getNewFilmes(titulo) {
     const url = `https://www.omdbapi.com/?t=${titulo}pikey=790af7bcz`
     fetch(url) // acessando a rota
-    .then(response => response.json()) //pega o resultado e transforma em json
-    .then(data => {
-        
-        //userBio.textContent = data.bio
-        capa.src = data.Poster
-        titulo.textContent = data.Title
-        descricao.textContent = data.Trama
+        .then(response => response.json()) //pega o resultado e transforma em json
+        .then(data => {
 
-
-
-    })
+            //userBio.textContent = data.bio
+            movieTitle.textContent = data.Title
+            movieYear.textContent = data.Year
+            movieGenre.textContent = data.Genre
+            movieRuntime.textContent = data.Runtime
+            moviePlot.textContent = data.Plot
+            moviePoster.src = data.Poster
+            
+        })
+        console.log(data)
 }
+
+movieForm.addEventLisenter('submit', function(e) {
+    getMovieInfos(title.value)
+    e.preventDefault();
+});
 
 getNewFilmes()
